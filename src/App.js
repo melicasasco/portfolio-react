06baf1ce66c363f92146827cf8595385d9106525
Melanie Casasco <melaniecasasco@gmail.com>
2022-02-02 15:54:22 -0300

    fix menu size

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import SobreMi from './components/sobremi/Sobremi';
 import Portfolio from './components/portfolio/Portfolio';
 import Contact from './components/contact/Contact';
 import Menu from "./components/menu/Menu";
-import { BrowserRouter } from "react-router-dom";
+
+
 
 import './app.scss';
 import { useState } from 'react';
@@ -13,19 +14,19 @@ function App() {
 
   const [menuOpen, setMenuOpen] = useState (false);
   return (
-<BrowserRouter>
+
     <div className="app">
-     <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-     <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
      <div className="sections">
         <Intro/>
         <SobreMi />
-        <Portfolio />
+        <Portfolio  />
         <Contact />
 
      </div>
     </div>
-    </BrowserRouter>
+
   );
 }
 
--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import PortfolioList from "../portfolioList/PortfolioList";
 import "./Portfolio.scss";
-import { BrowserRouter } from "react-router-dom";
-import { Link } from "react-router-dom";
 
 import {
 
